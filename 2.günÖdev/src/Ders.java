0914000e84d4e6adfb151ad16de7260b23263ad2
ZeynepAcarli <83496608+ZeynepAcarli@users.noreply.github.com>
2021-05-04 16:48:01 +0300

    deneme

---
+++ 2.günÖdev/src/Ders.java
@@ -0,0 +1,19 @@
+
+public class Ders {
+	
+	public Ders(int id, String name, String description,int price) {
+		
+		this.id=id;
+		this.name=name;
+		this.description=description;
+		this.price=price;
+	}
+	
+	int id;
+	String name;
+	String description;
+	int price;
+	
+	
+
+}
---
+++ 2.günÖdev/src/DersManager.java
@@ -0,0 +1,12 @@
+
+public class DersManager {
+	
+	public void Kaydol(Ders ders) {
+		System.out.println(ders.name + "ne kaydolundu.");
+	}
+	
+	public void Ara(Ders ders) {
+		System.out.println(ders.name + " arandı.");
+	}
+
+}
