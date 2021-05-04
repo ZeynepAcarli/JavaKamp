be2ae6bf7dfe54ebb45364e32a70a8d3e20a90da
ZeynepAcarli <83496608+ZeynepAcarli@users.noreply.github.com>
2021-05-04 17:12:19 +0300

    denme

---
+++ 2.GünÖdev/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Ders ders1= new Ders(1, "Java Dersi" , "21 Nisan",  0);
+		Ders ders2= new Ders(2, "C# Dersi", "9 Ocak" , 0);
+
+		DersManager dersManager = new DersManager();
+		dersManager.Kaydol(ders1);
+
+
+		DersManager dersManager2 = new DersManager();
+		dersManager2.Ara(ders2);
+
+
+	}
+	}
+
+
