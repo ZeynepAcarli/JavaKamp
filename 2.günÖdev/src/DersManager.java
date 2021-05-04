6584d2fffab2b0242063252c6de4b50df7d65914
ZeynepAcarli <zeynepa123456@gmail.com>
2021-05-04 16:23:28 +0300

    final

--- 2.günÖdev/src/DersManager.java
+++ 2.günÖdev/src/DersManager.java
@@ -1,6 +1,12 @@
 
 public class DersManager {
 	
-	public void Kaydol() {}
+	public void Kaydol(Ders ders) {
+		System.out.println(ders.name + "ne kaydolundu.");
+	}
+	
+	public void Ara(Ders ders) {
+		System.out.println(ders.name + " arandı.");
+	}
 
 }
